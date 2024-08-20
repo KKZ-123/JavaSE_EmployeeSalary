c8c6360bc3aff54419c7f03856b31877186d0ce8
Kgkhant Zaw <91204271+KKZ-123@users.noreply.github.com>
2024-08-20 23:55:40 +0630

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,41 @@
+package salary_mangaement_model;
+
+public class Employee {
+
+	private String name;
+	private int employeeID;
+	private double basicSalary;
+	private static int count = 0;
+	
+	public Employee(String name, int employeeID, double basicSalary) {
+		this.name = name;
+		this.employeeID = employeeID;
+		this.basicSalary = basicSalary;
+		
+		count++;
+	}
+	
+	public double calculateSalary() {
+		
+		return this.basicSalary;
+	}
+	
+	
+	public double getBasicSalary() {
+	
+		return this.basicSalary;
+	}
+	
+	public static int getCount() {
+		
+		return count;
+	}
+	
+	public void display() {
+		
+		System.out.println("Employee name : " + this.name);
+		System.out.println("Employee ID : " + this.employeeID);
+		System.out.println("Basic Salary: " + this.basicSalary);
+		System.out.println("Actual Salary : " + this.calculateSalary() + "\n");
+	}
+}
